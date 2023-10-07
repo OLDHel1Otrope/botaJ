2f21be075579a1452ed0178a92c04b1b4cdb8477
utkarsh pathak <76955280+OLDHel1Otrope@users.noreply.github.com>
2023-10-07 21:38:13 +0530

    Create javabegg.java

---
+++ javabegg.java
@@ -0,0 +1,152 @@
+package firstproject;
+
+import java.util.Scanner;
+import java.util.Random;		//pseudo random code
+
+
+//import javax.swing.JOptionPane;
+
+public class Main {
+
+	static public void main(String args[]) {
+		
+		/*System.out.print("hello fjc \\");
+		int x=50;
+		float y=12.457f;
+		System.out.println(x/y);
+		system.out.println((double)x/y);
+		System.out.println("the double precision value is "+(double)x/y);
+		byte a=12.7;
+		System.out.println(a);
+		a++;
+		System.out.println(a);
+		float ab= 20.02f;
+		double ac=ab;
+		boolean ad=false;
+		System.out.println(ab);
+		System.out.println(""+ad+ac);
+		char symbol='@';
+		System.out.println(symbol);
+		System.out.println(String.valueOf(a)); no idea
+		for(int x=0;x<256;x++) {
+		}
+		String ae="this is my first string";
+		System.out.println(ae);
+		String a="first";
+		String b="second";
+		String temp;
+		System.out.println(a+" "+b);
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println(a+" "+b);
+		
+		
+		Scanner firstscan =new Scanner(System.in);
+		System.out.println("what is your name?");
+		String name=firstscan.nextLine();
+		System.out.println("how old are you?");
+		int age=firstscan.nextInt();
+		System.out.println("what is your fruit?");
+		firstscan.nextLine();
+		String fruit=firstscan.nextLine();
+		System.out.println("your name is "+name+" and "+age+" is really old, and "+fruit+" is really distasteful");
+		int a=5;
+		System.out.println(++a);
+		System.out.println(a);*/
+		
+		
+		/*String name= JOptionPane.showInputDialog("enter your name");
+		JOptionPane.showMessageDialog(null,"hello "+name);
+		
+		int age=Integer.parseInt(JOptionPane.showInputDialog("enter your age"));
+		JOptionPane.showMessageDialog(null,"you are "+age+" years old");
+		
+		double marks=Double.parseDouble(JOptionPane.showInputDialog("enter your percentage"));
+		JOptionPane.showMessageDialog(null,"your marks are "+marks);*/
+		
+		
+		/*Scanner scanf=new Scanner(System.in);
+		double base=scanf.nextDouble();
+		scanf.nextLine();
+		double perpendicular=scanf.nextDouble();
+		scanf.nextLine();
+		double x=Math.sqrt((perpendicular*perpendicular)+(base*base));
+		System.out.println(x);	
+		scanf.close();*/
+		
+		
+		//Random random=new Random();
+		//int value=random.nextInt(7);
+		//System.out.println(value);
+		
+		//double value2=random.nextDouble(6);
+		//double value3=random.nextDouble(5);//only gives value between o and 1
+		//boolean v4=random.nextBoolean();
+		//System.out.println(value2);
+		//System.out.println(value3);
+		//System.out.println(v4);
+		
+		
+		/*for(int i=0;i<256;i++) {
+			System.out.print((char)i+"  ");
+			System.out.println(i);
+		}*/
+		
+		
+		/*
+		int a=15;
+		if(a>=15) {
+			System.out.println("First block runs");
+		}
+		else if(a==15) {
+			System.out.println("this will never run");
+		}
+		else {
+			System.out.println("nope");
+		}*/
+		
+		
+		/*String day="Friday";
+		switch(day) {
+			case "sunday": System.out.println("itsss sundayyyyy");
+			break;
+			case "Friday": System.out.println("FRIDAYYYYYY");
+			break;
+			default: System.out.println("boohoo");
+		}*/
+		
+		
+		
+		/*int value=6;
+		if(value>=1&&value<10) {
+			System.out.println("satisfies the expression");
+		}
+		else {
+			System.out.println("dosent");
+		}*/
+		
+		
+		/*System.out.println("you are playing a game, press w a s d to quit");
+		Scanner scanner=new Scanner(System.in);
+		String a=scanner.next();
+		if(a.equals("a")||a.equals("b")||a.equals("c")||a.equals("d"))
+		{
+			System.out.println("game ended");
+		}
+		else {
+			System.out.println("not recognized");
+		}
+		scanner.close();*/
+		
+		/*Scanner scanner=new Scanner(System.in);
+		String name="";
+		while(name.isBlank()) {
+			System.out.println("Enter your name");
+			name= scanner.nextLine();
+		}
+		scanner.close();
+		System.out.println("Hello "+name);
+		*/
+	}
+}
