868ee9604a4b9fd95836814aeaf19acd62eb3ab2
utkarsh pathak <76955280+OLDHel1Otrope@users.noreply.github.com>
2023-10-07 11:15:29 +0530

    Create variablesdatatypes.java

---
+++ variablesdatatypes.java
@@ -0,0 +1,31 @@
+//tryingtounderstandvariablesanddatatypes
+package firstproject;
+
+public class Main {
+
+	static public void main(String args[]) {
+		//System.out.print("hello fjc \\");
+		//int x=50;
+		//float y=12.457f;
+		//System.out.println(x/y);
+		//system.out.println((double)x/y);
+		//System.out.println("the double precision value is "+(double)x/y);
+		//byte a=12.7;
+		//System.out.println(a);
+		//a++;
+		//System.out.println(a);
+		//float ab= 20.02f;
+		//double ac=ab;
+		//boolean ad=false;
+		//System.out.println(ab);
+		//System.out.println(""+ad+ac);
+		//char symbol='@';
+		//System.out.println(symbol);
+		//System.out.println(String.valueOf(a)); //no idea
+		//for(int x=0;x<256;x++) {
+			
+		//}
+		//String ae="this is my first string";
+		//System.out.println(ae);
+	}
+}
